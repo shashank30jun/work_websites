c08881df4ca19a3c0ce67c8108b74e72a37dbff8
shashank <shashank.30jun@gmail.com>
2026-07-26 15:08:36 +0530

    initials setup

--- submit/submit.php
+++ submit/submit.php
@@ -10,10 +10,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $u_category = filter_var($_POST["category"], FILTER_SANITIZE_STRING);
   $u_desc = filter_var($_POST["desc"], FILTER_SANITIZE_STRING);
   $msgToClient = array(
-    'from'    => 'Yagnesh L P <admin@yagneshlp.me>',
+    'from'    => 'Shashank L P <admin@yagneshlp.me>',
     'to'      => $u_email ,
     'subject' => 'RE: ' . $u_category ,
-    'html'    => '<html>' .  '<body>' .     '  <h1> Hi '. $u_name . '</h1>' .     ' <br>Thanks for your interest in my services, I will get back to you within 24 Hours <br> <br> Yours Truly, <br> Yagnesh L P' .    ' </body>' .    '</html>'
+    'html'    => '<html>' .  '<body>' .     '  <h1> Hi '. $u_name . '</h1>' .     ' <br>Thanks for your interest in my services, I will get back to you within 24 Hours <br> <br> Yours Truly, <br> Shashank L P' .    ' </body>' .    '</html>'
   );
   $mgClient->messages()->send($domain, $msgToClient);
   $msgToMe = array(
