0ebecd9c3af26cb34a9c2d16a3120399648b8474
shashank <shashank.30jun@gmail.com>
2026-07-26 15:42:33 +0530

    corrected few errors

--- submit/submit.php
+++ submit/submit.php
@@ -2,23 +2,23 @@
 
 require_once 'vendor/autoload.php';
 use Mailgun\Mailgun;
-$mgClient = Mailgun::create('apicodegoeshere', 'https://api.mailgun.net/v3/ylp.wtf');
-$domain = "yagneshlp.me";
+$mgClient = Mailgun::create('apicodegoeshere', 'https://api.mailgun.net/v3/shashank.wtf');
+$domain = "shashank.wtf";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $u_name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
   $u_email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
   $u_category = filter_var($_POST["category"], FILTER_SANITIZE_STRING);
   $u_desc = filter_var($_POST["desc"], FILTER_SANITIZE_STRING);
   $msgToClient = array(
-    'from'    => 'Shashank L P <admin@yagneshlp.me>',
+    'from'    => 'Shashank <admin@shashank.wtf >',
     'to'      => $u_email ,
     'subject' => 'RE: ' . $u_category ,
     'html'    => '<html>' .  '<body>' .     '  <h1> Hi '. $u_name . '</h1>' .     ' <br>Thanks for your interest in my services, I will get back to you within 24 Hours <br> <br> Yours Truly, <br> Shashank L P' .    ' </body>' .    '</html>'
   );
   $mgClient->messages()->send($domain, $msgToClient);
   $msgToMe = array(
-    'from'    => 'Postman <postman@yagneshlp.me>',
-    'to'      => 'yagneshlp@ylp.wtf' ,
+    'from'    => 'Postman <postman@shashank.wtf>',
+    'to'      => 'shashank@shashank.wtf' ,
     'subject' => 'An enquiry by ' . $u_name ,
     'text'    => 'Hey, someone Enquired about ' . $u_category . '. Their email is: ' . $u_email . '. Their needs are : ' . $u_desc
   );
